d794cfe0a26f5d6c602c4506b9607ca2cd6a2254
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-03-01 17:29:53 -0500

    update

--- rename2.bash
+++ rename2.bash
@@ -3,7 +3,7 @@
 for fname in *; do
   name="${fname%\.*}"
   extension="${fname#$name}"
-  newname="${name//./_}"
+  newname="${name//_/.}"
   newfname="$newname""$extension"
   if [ "$fname" != "$newfname" ]; then
     echo mv "$fname" "$newfname"
