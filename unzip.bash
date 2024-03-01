178191f7e04cf5cdd9ead5313066f1d5e3d5e655
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2024-03-01 09:29:04 -0500

    Updated

--- unzip.bash
+++ unzip.bash
@@ -7,7 +7,22 @@
 #SBATCH -o unzip-%j.out             # Output file for this job.
 #SBATCH --account=coa_mki314_uksr     # Project allocation account name (REQUIRED)
 
-cd /scratch/jwme229/unzip_test
-for i in "*.gz" ; do
-   gunzip $i ;
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   Usage: ./unzip.bash folder
+   folder	Path to the folder containing the zipped reads. The zipped reads must have .gz file type.
+      " >&2 ;
+   exit 1 ;
+fi ;
+
+# Specify the directory containing your gzipped files
+gzipped_dir="$1"
+
+# Iterate over each .gz file in the specified directory
+for i in "$gzipped_dir"/*.gz; do
+    
+    # Use the gunzip command to unzip the file
+    gunzip "$i"
+
+    echo "Unzipped: $i"
 done
