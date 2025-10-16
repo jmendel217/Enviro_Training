e0931339da71095e969475d8da260bb9b53431b8
jmendel217 <156953724+jmendel217@users.noreply.github.com>
2025-10-16 12:37:35 -0400

    Create copy_RUNME.bash

---
+++ copy_RUNME.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [[ "$1" == "" || "$1" == "-h" ]] ; then
+   echo "
+   Usage: ./RUNME.bash [source] [destination] [queue] [QOS]
+   
+   source      Path to the folder containing the files you want to copy
+   destination Path to the folder where you want files to be copied to
+   partition   Select a partition (if not provided, coa_mki314_uksr will be used)
+   qos         Select a quality of service (if not provided, normal will be used)
+
+   " >&2 ;
+   exit 1 ;
+fi ;
+
+DEST=$2
+if [[ "$DEST" == "" ]] ; then
+   exit 1 ;
+fi ;
+
+QUEUE=$3
+if [[ "$QUEUE" == "" ]] ; then
+   QUEUE="coa_mki314_uksr"
+fi ;
+
+QOS=$4
+if [[ "$QOS" == "" ]] ; then
+   QOS="normal"
+fi ;
+
+SRCE=$(readlink -f $1)
+pac=$(dirname $(readlink -f $0))
+cwd=$(pwd)
+
+#---------------------------------------------------------
+
+cd $SRCE
+
+for i in $SRCE/*.fastq ; do
+   b=$(basename $i .fastq)
+   OPTS="SAMPLE=$b,FOLDER=$SRCE"
+   # Launch job
+   sbatch --export="$OPTS" -J "Copy-$b" --account=$QUEUE --partition=$QOS --error "$DEST"/"Copy-$b"-%j.err -o "$DEST"/"Copy-$b"-%j.out  $pac/run.pbs | grep .;
+done
+
+#---------------------------------------------------------
+
+echo "Done: $(date)." ;
